417dae87fab988e3b92e9ec7af6a2704afe2bab0
Rohit Patnaik <quanticle@quanticle.net>
2022-12-09 05:33:08 -0600

    Removed unnecessary .iml file

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject fix-youtube-mtime "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "GPL 3"
+            :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-http "3.12.3"]
                  [mockery "0.1.4"]
